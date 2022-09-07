ab7a5e4ebb6bbc48326ce20b5b4c5eb5079c8486
Jonathas Ribeiro Azer <112482377+j-azer@users.noreply.github.com>
2022-09-07 21:19:38 +0100

    novo doc
    exercício de estrutura sequencial

---
+++ Estrutura_Seq_EX5.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Globalization;
+
+Console.WriteLine("----------------------");
+Console.WriteLine("| Calculo de Salario |");
+Console.WriteLine("----------------------");
+
+int NF, HT;
+double Sal, VPH;
+
+Console.WriteLine("Digite o Nº do funcionário: ");
+NF = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite a quantidade de horas trabalhadas: ");
+HT = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o valor que este funcionario recebe por hora: ");
+VPH = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.WriteLine();
+
+Sal = HT * VPH;
+
+Console.WriteLine("Funcionário Numero: " + NF);
+Console.WriteLine();
+Console.WriteLine("O salario e U$ " + Sal.ToString("F2", CultureInfo.InvariantCulture));
+
