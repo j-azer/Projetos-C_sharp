953a624ddb791d8c6b975619e17e8ad9df4455cd
Jonathas Ribeiro Azer <112482377+j-azer@users.noreply.github.com>
2022-09-08 13:16:30 +0100

    inclusao arq
    Inclusão EX6 e EX7

---
+++ Estrutura_Seq_EX6.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
+
+Console.WriteLine("------------------------");
+Console.WriteLine("| Cupom Fiscal Simples |");
+Console.WriteLine("------------------------");
+
+int codigo, codigo1, qtd, qtd1;
+double valorUnit, valorUnit1, valorTotal1, valorTotal2, valorTotalF;
+
+Console.WriteLine("Escreva o codigo da peça: ");
+codigo = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Escreva a quantidade desta peça: ");
+qtd = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Escreva o valor unitário desta peça: ");
+valorUnit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.WriteLine();
+
+Console.WriteLine("Escreva o codigo da peça: ");
+codigo1 = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Escreva a quantidade desta peça: ");
+qtd1 = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Escreva o valor unitário desta peça: ");
+valorUnit1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.WriteLine();
+
+valorTotal1 = qtd * valorUnit;
+
+valorTotal2 = qtd1 * valorUnit1;
+
+valorTotalF = valorTotal1 + valorTotal2;
+
+Console.Clear();
+Console.WriteLine("----------------");
+Console.WriteLine("| Cupom Fiscal |");
+Console.WriteLine("----------------");
+Console.WriteLine();
+Console.WriteLine($"|CodigoPeça - {codigo} | Qtd - {qtd} | ValorUnit - U$ {valorUnit.ToString("F2", CultureInfo.InvariantCulture)} |");
+Console.WriteLine($"|CodigoPeça - {codigo1} | Qtd - {qtd1} | ValorUnit - U$ {valorUnit1.ToString("F2", CultureInfo.InvariantCulture)} |");
+Console.WriteLine();
+Console.WriteLine("VALOR A PAGAR - U$ " + valorTotalF.ToString("F2", CultureInfo.InvariantCulture));
---
+++ Estrutura_Seq_EX7.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Globalization;
+
+Console.WriteLine("----------------------------");
+Console.WriteLine("| AREA Figuras GEOMETRICAS |");
+Console.WriteLine("----------------------------");
+
+double A, B, C, areaTri, areaC, areaQ, areaTra, areaR;
+double pi = 3.14159;
+
+
+Console.WriteLine("Digite um valor para A: ");
+A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.WriteLine("Digite um valor para B: ");
+B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.WriteLine("Digite um valor para C: ");
+C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+areaTri = (A * C) / 2.0;
+areaC = pi * C * C;
+areaTra = (A + B) * C / (2.0);
+areaQ = B * B;
+areaR = A * B;
+
+Console.Clear();
+Console.WriteLine("----------------------------");
+Console.WriteLine("| AREA Figuras GEOMETRICAS |");
+Console.WriteLine("----------------------------");
+Console.WriteLine();
+Console.WriteLine("Para A = " +A.ToString("F1", CultureInfo.InvariantCulture)+ ", B = " +B.ToString("F1", CultureInfo.InvariantCulture) + ", C = "+C.ToString("F1", CultureInfo.InvariantCulture)+ " e Pi = "+pi.ToString("F5", CultureInfo.InvariantCulture));
+Console.WriteLine();
+Console.WriteLine("A área do triângulo retângulo que tem A por base e C por altura: ");
+Console.WriteLine($"Area TRIANGULO = {areaTri.ToString("F3", CultureInfo.InvariantCulture)}");
+Console.WriteLine();
+Console.WriteLine("A área do círculo de raio C: ");
+Console.WriteLine($"Area CIRCULO = {areaC.ToString("F3", CultureInfo.InvariantCulture)}");
+Console.WriteLine();
+Console.WriteLine("A área do trapézio que tem A e B por bases e C por altura: ");
+Console.WriteLine($"Area TRAPEZIO = {areaTra.ToString("F3", CultureInfo.InvariantCulture)}");
+Console.WriteLine();
+Console.WriteLine("A área do quadrado que tem lado B: ");
+Console.WriteLine($"Area QUADRADO = {areaQ.ToString("F3", CultureInfo.InvariantCulture)}");
+Console.WriteLine();
+Console.WriteLine("A área do retângulo que tem lados A e B: ");
+Console.WriteLine($"Area RETANGULO = {areaR.ToString("F3", CultureInfo.InvariantCulture)}");
+Console.WriteLine();
