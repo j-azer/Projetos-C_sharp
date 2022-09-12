2ec47df4f80c04e3d2ec9f334a21485951d6d1b1
Jonathas Ribeiro Azer <112482377+j-azer@users.noreply.github.com>
2022-09-12 18:57:29 +0100

    nova resolucao
    Resolução e enunciado EX6

---
+++ Estrutura_Cond_EX6.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+using System.Globalization;
+
+
+double numero; 
+
+Console.WriteLine("Digite um numero qualquer: ");
+numero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+if (numero < 0.0 || numero > 100.0)
+{
+    Console.WriteLine("Fora de intervalo");
+}
+else if (numero <= 25.0)
+{
+    Console.WriteLine("Intervalo [0,25]");
+}
+else if (numero <= 50.0)
+{
+    Console.WriteLine("Intervalo (25,50]");
+}
+else if (numero <= 75.0)
+{
+    Console.WriteLine("Intervalo (50,75]");
+}
+else
+{
+    Console.WriteLine("Intervalo (75,100]");
+}
+
--- README.md
+++ README.md
@@ -52,7 +52,7 @@
 <p>| 4         Torrada Simples     U$ 2.00 |
 <p>| 5         Refrigerante        U$ 1.50 |
 <p>
-<p>
+ <h4>Estrutura_Cond_EX6:</h4> Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos seguintes intervalos ([0,25], (25,50], (50,75], (75,100]) este valor se encontra. Obviamente se o valor não estiver em nenhum destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”.
 <p>
 <p>
 <p>
