68505af321c72a809c700faa29bca79c564adcf4
Jonathas Ribeiro Azer <112482377+j-azer@users.noreply.github.com>
2022-09-13 18:44:44 +0100

    novo exercico e resolucao

---
+++ Estrutura_Cond_EX7.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
+using System;
+
+double x, y;
+string resp;
+
+do
+{
+    Console.WriteLine("----------------------------------");
+    Console.WriteLine("|  Identificador de Coordenadas  |");
+    Console.WriteLine("----------------------------------");
+    Console.WriteLine();
+
+    Console.WriteLine("Digite a coordenada do eixo X");
+    x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    Console.WriteLine();
+    Console.WriteLine("Digite a coordenada do eixo Y");
+    y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    Console.WriteLine();
+
+
+    if (x == 0 && y! > 0)
+    {
+        Console.WriteLine("Encontra-se no EIXO Y.");
+    }
+    else if (x! > 0 && y == 0)
+    {
+        Console.WriteLine("Encontra-se no EIXO X.");
+    }
+    else if (x > 0 && y > 0)
+    {
+        Console.WriteLine("Encontra-se no QUADRANTE 1.");
+    }
+    else if (x < 0 && y < 0)
+    {
+        Console.WriteLine("Encontra-se no QUADRANTE 3.");
+    }
+    else if (x > 0 && y < 0)
+    {
+        Console.WriteLine("Encontra-se no QUADRANTE 4.");
+    }
+    else if (x < 0 && y > 0)
+    {
+        Console.WriteLine("Encontra-se no QUADRANTE 2.");
+    }
+    else
+    {
+        Console.WriteLine("Encontra-se na ORIGEM.");
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("Quer continuar? [S/N]");
+    resp = Console.ReadLine();
+    Console.Clear();
+}
+
+while (resp.ToUpper() == "S");
+
+Console.WriteLine("----------------------------------");
+Console.WriteLine("|  Obrigado e até a Proxima !!!  |");
+Console.WriteLine("----------------------------------");
+Console.WriteLine();
--- README.md
+++ README.md
@@ -54,6 +54,10 @@
 <p>
  <h4>Estrutura_Cond_EX6:</h4> Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos seguintes intervalos ([0,25], (25,50], (50,75], (75,100]) este valor se encontra. Obviamente se o valor não estiver em nenhum destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”.
 <p>
+ <h4>Estrutura_Cond_EX7:</h4> Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0). Se o ponto estiver na origem, escreva a mensagem “Origem”. Se o ponto estiver sobre um dos eixos escreva “Eixo X” ou “Eixo Y”, conforme for a 
+situação.
+<p>
+<p>
 <p>
 <p>
 <p>
