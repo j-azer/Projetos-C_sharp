9defbf0eac473207e8e5607ed2bc28c8a0b5932f
Jonathas Ribeiro Azer <112482377+j-azer@users.noreply.github.com>
2022-09-20 18:37:37 +0100

    new resolution repeat

---
+++ Estrutura_Rep_EX1.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
+
+
+
+Console.WriteLine("------------------------");
+Console.WriteLine("| Verificador de Senha |");
+Console.WriteLine("------------------------");
+Console.WriteLine();
+Console.WriteLine("Bem vindo ao sistema !!! Digite sua senha de acesso: ");
+int senha = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Clear();    
+
+while (senha != 2002)
+{
+    Console.WriteLine("------------------------");
+    Console.WriteLine("| Verificador de Senha |");
+    Console.WriteLine("------------------------");
+    Console.WriteLine();
+    Console.WriteLine("Senha inválida... Digite novamente: ");
+    senha = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    Console.Clear();
+}
+Console.WriteLine("------------------------");
+Console.WriteLine("| Verificador de Senha |");
+Console.WriteLine("------------------------");
+Console.WriteLine();
+Console.WriteLine("Acesso Permitido !!!");
---
+++ Estrutura_Rep_EX2.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+Console.WriteLine("--------------------------------");
+Console.WriteLine("| Identificador de Coordenadas |");
+Console.WriteLine("--------------------------------");
+Console.WriteLine();
+Console.WriteLine("Digite dois valores de coordenadas separados por espaço:");
+string[] valores = Console.ReadLine().Split(' ');
+int x = int.Parse(valores[0]);
+int y = int.Parse(valores[1]);
+Console.WriteLine();
+
+while (x != 0 && y != 0)
+{
+    if (x > 0 && y > 0)
+    {
+        Console.WriteLine("1º Quadrante");
+    }
+    else if (x < 0 && y > 0)
+    {
+        Console.WriteLine("2º Quadrante");
+    }
+    else if (x < 0 && y < 0)
+    {
+        Console.WriteLine("3º Quadrante");
+    }
+    else
+    {
+        Console.WriteLine("4º Quadrante");
+    }
+    Console.WriteLine();
+    Console.WriteLine("Digite dois valores de coordenadas separados por espaço:");
+    valores = Console.ReadLine().Split(' ');
+    x = int.Parse(valores[0]);
+    y = int.Parse(valores[1]);
+    Console.WriteLine();
+
+}
+Console.Clear();
+Console.WriteLine("---------------------");
+Console.WriteLine("| Valor Nulo... FIM |");
+Console.WriteLine("---------------------");
